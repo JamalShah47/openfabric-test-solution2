b9609e3dff9d38fa63c6d9d5c41d799e557b8f1d
Jamal Shah <75212149+JamalShah47@users.noreply.github.com>
2022-12-21 08:32:21 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+import os
+import warnings
+from ontology_dc8f06af066e4a7880a5938933236037.simple_text import SimpleText
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+from openfabric_pysdk.context import OpenfabricExecutionRay
+from openfabric_pysdk.loader import ConfigClass
+from time import time
+import json
+sciencebot = ChatBot("Science Bot", read_only = False)
+trainBot = ChatterBotCorpusTrainer(sciencebot)
+trainBot.train ("chatterbot.corpus.english.science")
+
+
+############################################################
+# Callback function called on update config
+############################################################
+def config(configuration: ConfigClass):
+    with open("config/execution.json", "w") as jsonfile:
+        configuration_obj = json.load(jsonfile)
+        configuration_obj ["config_class"]  = configuration
+        json.dump(configuration_obj, jsonfile)
+    pass
+
+
+############################################################
+# Callback function called on each execution pass
+############################################################
+def execute(request: SimpleText, ray: OpenfabricExecutionRay) -> SimpleText:
+    output = []
+    for text in request.text:
+        response = sciencebot.get_response(text)
+        output.append(response)
+
+    return SimpleText(dict(text=output))
